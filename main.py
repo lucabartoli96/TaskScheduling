1ab62ba26f4685be0f04051d1d4fcffb3d93a50f
lucabartoli96 <lucabartoli1996@gmail.com>
2018-04-11 18:45:17 +0200

    Solved LOG problem

--- main.py
+++ main.py
@@ -29,17 +29,17 @@ def is_pow_of_two(n):
     return math.log(n, 2) == math.floor(math.log(n, 2))
     
 def main(argv): 
-#
-#    N = insert_int('N := ')
-#    m = insert_int('m := ')
-#    
-#    while(m < N):
-#        print "m must be bigger or equal to N"
-#        m = insert_int('m := ')
-#    
-#    w = list()
-#    for i in range(m):
-#        w.append(insert_int('t_%d := ' % i))
+
+    N = insert_int('N := ')
+    m = insert_int('m := ')
+    
+    while(m < N):
+        print "m must be bigger or equal to N"
+        m = insert_int('m := ')
+    
+    w = list()
+    for i in range(m):
+        w.append(insert_int('t_%d := ' % i))
 
 #    N = 8
 #    m = 12
@@ -47,11 +47,11 @@ def main(argv):
 #    N = 4
 #    m = 9
 #    task = [2, 6, 2, 2, 1, 1, 2, 2, 2]
-    N = 3
-    m = 5
-    w = [3, 2, 4, 2, 4]
+#    N = 3
+#    m = 5
+#    w = [3, 2, 4, 2, 4]
 
-    LOG = argv[1] and argv[1] == "LOG"
+    LOG = argv and len(argv)>1 and argv[1] == "LOG"
 
     if is_pow_of_two(N):
         print_schedule("Bisection Algorithm", w, bisection(N, m, w, LOG))
